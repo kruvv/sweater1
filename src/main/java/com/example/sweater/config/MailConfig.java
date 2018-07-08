c6730c8c4221ab6e23c1d25255d1ed39a95cffe9
kruvv <kruvv@mail.ru>
2018-07-08 15:08:04 +0300

    add properties mail

---
+++ src/main/java/com/example/sweater/config/MailConfig.java
@@ -0,0 +1,22 @@
+package com.example.sweater.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.JavaMailSenderImpl;
+
+@Configuration
+public class MailConfig {
+    @Value("S{spring.mail.host}")
+    private String host;
+    @Bean
+    public JavaMailSender getMailSender() {
+        JavaMailSenderImpl mailSender = new JavaMailSenderImpl();
+
+
+
+
+        return mailSender;
+    }
+}
